0410605410316fc4c8d9a90edab6c4cf0c24b10b
Vladimir Magaziy <vmagaziy@gmail.com>
2015-11-26 15:53:20 +0200

    Initial revision of missing file defining API a sample API identifier and hash just to make it work. These values are  to be replaced with actual ones to be used in a specific client.

---
+++ config.h
@@ -0,0 +1,3 @@
+// Just to make it work, to be replaced with actual values to be used in a specific client
+#define SETUP_API_ID(apiId) apiId = 43058;
+#define SETUP_API_HASH(apiHash) apiHash = @"1f4ab41522ddeef0f9a2a9e0095fcf38";
